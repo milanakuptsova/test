9ece57e384e74dbf90296f09b341607eaa0291db
milanakuptsova <114679500+milanakuptsova@users.noreply.github.com>
2025-05-15 22:05:52 +0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,10 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Приложение работает на Render!"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=100
